ec91fec01d232daa3346c6840b6ca7e042e5dbe5
Matt Bernhardt <mjbernha@mit.edu>
2022-03-31 16:56:32 -0400

    Fix code indenting
    Gotta keep phpcs happy

--- widget.php
+++ widget.php
@@ -20,7 +20,7 @@ if ( $urgent->have_posts() ) {
 	while ( $urgent->have_posts() ) {
 		// Add the 'form-invalid' class to all urgent post listings so they appear red.
 		$urgent->the_post();
-	?>
+		?>
 		<tr class="urgent">
 			<td class="row-title">
 				<a href="<?php echo esc_url( get_edit_post_link() ); ?>">
@@ -29,7 +29,7 @@ if ( $urgent->have_posts() ) {
 			</td>
 			<td><?php echo esc_html( get_the_author() ); ?></td>
 		</tr>
-	<?php
+		<?php
 	}
 } else {
 	?>
@@ -43,7 +43,7 @@ if ( $urgent->have_posts() ) {
 if ( $pending->have_posts() ) {
 	while ( $pending->have_posts() ) {
 		$pending->the_post();
-	?>
+		?>
 		<tr>
 			<td class="row-title">
 				<a href="<?php echo esc_url( get_edit_post_link() ); ?>">
@@ -52,7 +52,7 @@ if ( $pending->have_posts() ) {
 			</td>
 			<td><?php echo esc_html( get_the_author() ); ?></td>
 		</tr>
-	<?php
+		<?php
 	}
 } else {
 	?>
